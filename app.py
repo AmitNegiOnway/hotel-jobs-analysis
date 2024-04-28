b0dfbe48ae622b6aa307704606bb6f80af92f554
amit negi <amitnegi@gmail.com>
2024-04-28 09:21:56 +0530

    this commit will be add side bar in our website

--- app.py
+++ app.py
@@ -4,6 +4,12 @@ from matplotlib import pyplot as plt
 df = pd.read_csv('new_data.csv')
 st.set_page_config(layout='wide',page_title='ONWAY.COM')
 
+def load_campany_analysis():
+    pass
+def load_country_analysis():
+    pass
+def load_jobstype_analysis():
+    pass
 
 
 st.title('WELCOME TO ONWAY HOPITALITY JOBS')
@@ -15,3 +21,33 @@ st.write('Welcome to our website, your gateway to the world of hospitality caree
 st.write('Whether you dream of managing a luxury hotel, crafting exquisite culinary creations, or orchestrating unforgettable events, our platform is designed to support your aspirations. Join us today and embark on your journey towards a fulfilling career in hospitality!')
 
 st.write("Through innovative web scraping technology, we gather job opportunities from renowned platforms like Hosco, ensuring that you have access to the latest openings in the hospitality sector. But we don't stop there. Our website goes beyond mere listings – we empower you with the tools for data analysis, helping you make informed decisions about your career path.Whether you dream of managing a luxury hotel, crafting exquisite culinary creations, or orchestrating unforgettable events, our platform is designed to support your aspirations. Join us today and embark on your journey towards a fulfilling career in hospitality!")
+
+
+
+st.sidebar.title('Startup Funding Analysis')
+option = st.sidebar.selectbox('select one', ['Country-Wise Analysis', 'Company-Wise Analysis', 'Jobs Type Analysis'])
+
+if option == 'Country-Wise Analysis':
+    st.sidebar.selectbox('select Country', sorted(df.country.str.strip().unique().tolist()))
+    btn0=st.sidebar.button('Countries analysis')
+
+
+    if btn0:
+        load_country_analysis()
+
+
+
+elif option == 'Company-Wise Analysis':
+    st.sidebar.selectbox('select Company', sorted(df.hotel_name.str.strip().unique().tolist()))
+    btn1=st.sidebar.button('companies detail')
+
+
+    if btn1:
+        load_campany_analysis()
+else:
+    selected_investor=st.sidebar.selectbox('select Jobs Type', sorted(df.job_type.str.strip().unique().tolist()))
+    btn2 = st.sidebar.button('Jobs Type Analysis ')
+    if btn2:
+        load_jobstype_analysis()
+
+
